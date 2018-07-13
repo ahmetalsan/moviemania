2cb8712df8832e4305958e6510a98fbdf8651a96
Ahmet Alsan <ahmetalsan@gmail.com>
2018-07-14 00:26:08 +0300

    design stage

---
+++ MovieMania/MovieCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieCell.swift
+//  MovieMania
+//
+//  Created by Ahmet Alsan on 14.07.2018.
+//  Copyright © 2018 Veoss. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+}
